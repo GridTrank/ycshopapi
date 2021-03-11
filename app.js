cb6a81748b9d3e12144e516d8a7f75ef5c62a6eb
lkzasd001 <kai.liu@miniso.com>
2021-03-11 13:38:50 +0800

    1231321313

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
 const express=require('express')
-const jwt = require("jsonwebtoken");
 const expressJWT = require('express-jwt');
 const app =express();
 app.use(express.static('public'))
-
 const bodyParser=require('body-parser')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}))
 
-var {PRIVITE_KEY} = require("./utils/store")
 app.all('*', function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST'); 
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization'); 
     res.setHeader("Content-Type", "application/json;charset=utf-8");
-    console.log(1111,res)
+
     next();
 })
 
 // 解密token
-
+var {PRIVITE_KEY} = require("./utils/store")
 app.use(expressJWT({
     secret: PRIVITE_KEY,
     algorithms:['HS256'],
@@ -29,14 +26,12 @@ app.use(expressJWT({
 
 // 中间件，处理请求中公用的方法
 // 针对rep或者res做一些操作
-app.all('/*',(err,req,res,next)=>{
-    
+app.use((err,req,res,next)=>{
     if (err.name === 'UnauthorizedError') {	
-      res.send({
-          code:401,
+      res.status(401).send({
+          code:100002,
           message:'登录超时'
       });
-      
       return
     }
     next()
